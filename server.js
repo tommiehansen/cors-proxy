0e5f62d9f91ff5bceafec83347f38588f42955e9
Claude Seguret <claude.seguret@laposte.fr>
2017-06-24 08:14:47 +0200

    Adapatation contexte + suppression header pour url

--- server.js
+++ server.js
@@ -13,28 +13,45 @@ app.all('*', function (req, res, next) {
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers') || 'origin, content-type, accept, location, code');
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
         res.send();
     } else {
-        var targetURL = req.header('Target-URL');
-        if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
-            return;
+        var targetURL = req.originalUrl.substr(1);
+        if ( targetURL !== "" && targetURL.indexOf("http://") !== 0 && targetURL.indexOf("https://") !== 0) {
+            targetURL = 'http://' + targetURL;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+        if (!targetURL) {
+           res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+           return;
+        }
+        // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        var headers = {};
+        if ( req.header('Authorization')) {
+            headers = {'Authorization': req.header('Authorization')};
+        }
+        // url: targetURL, + req.url
+        request({ 
+            url: targetURL, 
+            method: req.method, 
+            json: req.body, 
+            headers: headers,
+            strictSSL: false },
             function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
-                }
-//                console.log(body);
+                //console.log(error);
+                //console.log(response.statusCode);
+                //res.send(500, { error: error });
+                //if (error) {
+                    //console.error('error: ' + response.statusCode);
+                //}
+                //                console.log(body);
             }).pipe(res);
     }
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 9000);
 
 app.listen(app.get('port'), function () {
     console.log('Proxy server listening on port ' + app.get('port'));
