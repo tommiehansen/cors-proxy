ad9f3454ec631c46111a745e1af7469fe4584053
Claude Seguret <claude.seguret@laposte.fr>
2017-06-24 09:00:44 +0200

    suppression log, ajout favicon

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var express = require('express'),
     request = require('request'),
+    favicon = require('serve-favicon'),
     bodyParser = require('body-parser'),
+    path = require('path'),
     app = express();
 
 var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
 console.log('Using limit: ', myLimit);
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(bodyParser.json({limit: myLimit}));
 
 app.all('*', function (req, res, next) {
-    console.log(req.originalUrl);
+    
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
@@ -19,7 +23,7 @@ app.all('*', function (req, res, next) {
         // CORS Preflight
         res.send();
     } else {
-        console.log(req.originalUrl);
+        //console.log(req.originalUrl);
         var targetURL = req.originalUrl.substr(1);
         if ( targetURL !== "" && targetURL.indexOf("http://") !== 0 && targetURL.indexOf("https://") !== 0) {
             targetURL = 'http://' + targetURL;
@@ -34,7 +38,7 @@ app.all('*', function (req, res, next) {
             headers = {'Authorization': req.header('Authorization')};
         }
         // url: targetURL, + req.url
-        console.log(targetURL);
+        //console.log(targetURL);
         request({ 
             url: targetURL, 
             method: req.method, 
@@ -45,15 +49,15 @@ app.all('*', function (req, res, next) {
                 //console.log(error);
                 //console.log(response.statusCode);
                 //res.send(500, { error: error });
-                if (error) {
-                     console.log(error);
-                }
-                if (response) {
-                     console.log(response);
-                }
-                if (body) {
-                     console.log(body);
-                }
+                // if (error) {
+                //      console.log(error);
+                // }
+                // if (response) {
+                //      console.log(response);
+                // }
+                // if (body) {
+                //      console.log(body);
+                // }
                 //                console.log(body);
             }).pipe(res);
     }
